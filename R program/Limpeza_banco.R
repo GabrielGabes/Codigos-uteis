db500f9f25c48e26c006a712482f010cd4c1bc39
GabrielGabes <gabriel_s_anjos@yahoo.com>
2024-09-23 01:24:26 -0300

    ghvgvjv

--- R program/Limpeza_banco.R
+++ R program/Limpeza_banco.R
@@ -2,7 +2,24 @@
 
 
 ####################################################
-
+# Função para normalizar os nomes das colunas
+library(stringr)
+normalize_column_names <- function(df) {
+  # Remover acentuação
+  colnames(df) <- iconv(colnames(df), from = "UTF-8", to = "ASCII//TRANSLIT")
+  # Converter para minúsculas
+  colnames(df) <- tolower(colnames(df))
+  # Substituir espaços por underscores
+  colnames(df) <- str_replace_all(colnames(df), " ", "_")
+  # Remover caracteres especiais, mantendo apenas letras, números e underscores
+  colnames(df) <- gsub("[^a-z0-9_]", "", colnames(df))
+  # Substituir múltiplos underscores consecutivos por um único underscore
+  colnames(df) <- gsub("_+", "_", colnames(df))
+  # Remover underscores no início ou fim dos nomes de colunas
+  colnames(df) <- gsub("^_|_$", "", colnames(df))
+  
+  return(df)
+}
 
 ####################################################
 
--- R program/executar_sempre.R
+++ R program/executar_sempre.R
@@ -1,91 +1,20 @@
-#### cleaning desktop ####
-# source("~/Codigos úteis/R program/Limpeza_area_de_trabalho.R", echo=F)
-source("https://raw.githubusercontent.com/GabrielGabes/Codigos-uteis/main/R%20program/Limpeza_area_de_trabalho.R")
+#### Limpando desktop ####
 
+#source("https://raw.githubusercontent.com/GabrielGabes/Codigos-uteis/main/R%20program/Limpeza_area_de_trabalho.R")
+rm(list = ls()) # Limpar o ambiente de espaço de trabalho
+if(!is.null(dev.list())) dev.off() # Limpar a guia Gráficos 
+cat("\014") #Limpar o console: Ctrl + L
 
-
+######################################################################################
 
 #### Suprimir todos os warnings no script ####
 options(warn = -1)
 
-
-
+######################################################################################
 
 #### Facilitador de carregamento de pacotes ####
 if(!require(pacman)) install.packages("pacman")
 library(pacman)
-
-
-# dependents Packages
-pacman::p_load(
-  stats,
-  rlang,
-  dplyr,
-  janitor,
-  effsize,
-  caret,
-  DescTools,
-  car,
-  pROC,
-  MuMIn,
-  magrittr,
-  tidyr
-)
-#### Outros Pacotes ####
-pacman::p_load(
-  tidyr, #manipulação de dados #pivot_longer
-  tibble, #manipulação de dados
-  broom, #tem a ver com a bibli tidy()
-  devtools #criar e baixar pacotes de outros usuarios
-)
-
-
-
-
-## Criando dataframe ficticio para testes
-# source("~/Codigos úteis/Gerador de Dados Ficticios/dataframe.R", echo=F)
-source('https://raw.githubusercontent.com/GabrielGabes/Codigos-uteis/main/Gerador%20de%20Dados%20Ficticios/dataframe.R')
-
-
-
-
-#### Compilando arquivos .RMD ####
-pacman::p_load(knitr)
-knit("C:/Users/gabri/OneDrive/Documentos/Codigos úteis/R program/Funcoes.Rmd")
-#knit("C:/Users/gabri/OneDrive/Documentos/Codigos úteis/R program/Graficos_GGplot.Rmd")
-source('https://raw.githubusercontent.com/GabrielGabes/Codigos-uteis/main/R%20program/Graficos_GGplot.R')
-
-
-
-
-#### BIBLIOTECAS SEMPRE USADAS ####
-pacman::p_load(
-  readxl, #Importar arquvios excel ==> read_excel(".xlsx)
-  skimr, #resumo dos dados do dataframe => skim(df)
-  
-  reshape2, #manipulação de formato de dataframe
-  stringr, #manipulação de strings
-  vegan, #transformação de variaveis
-  forcats, #manipulação de factor
-  
-  # Outros pacotes
-  naniar, #analise de dados ausentes
-  DescTools #canivete suiço
-)
-
-
-
-
-'AMBIENTE PRONTO PARA TRABALHO'
-
-pacman::p_load(clipr) # captura dos dados => write_clip
-capture = function(tabela, col_names=TRUE, pontuacao=','){
-  tabela %>% print() %>% write_clip(dec = pontuacao, col.names = col_names)
-}
-
-
-
-
 
 #### Carregando meu pacote de analise ####
 # dependents Packages
@@ -103,6 +32,7 @@ pacman::p_load(
   magrittr,
   tidyr
 )
+#########################################
 
 escolha = 0
 if (escolha == 1){ ## MODO DIRETO
@@ -119,11 +49,8 @@ if (escolha == 1){ ## MODO DIRETO
 } else { ## MODO DRIBLANDO PROBLEMAS COM BLOQUEIO
   pacman::p_load(httr, xml2)
   
-  # URL da página de arquivos no GitHub
-  url <- "https://github.com/GabrielGabes/statgsa/tree/master/R"
-  
-  # Obtenha o conteúdo da página
-  page <- httr::GET(url)
+  url <- "https://github.com/GabrielGabes/statgsa/tree/master/R" # URL da página de arquivos no GitHub
+  page <- httr::GET(url) # Obtenha o conteúdo da página
   
   # Parse o HTML da página
   content <- content(page, "text")
@@ -144,3 +71,53 @@ if (escolha == 1){ ## MODO DIRETO
     source(file_url, local = TRUE)
   }
 }
+
+######################################################################################
+
+#### Outros Pacotes ####
+pacman::p_load(
+  tidyr, #manipulação de dados #pivot_longer
+  tibble, #manipulação de dados
+  broom, #tem a ver com a bibli tidy()
+  devtools #criar e baixar pacotes de outros usuarios
+)
+
+######################################################################################
+
+## Criando dataframe ficticio para testes
+# source("~/Codigos úteis/R program/Gerador de Dados Ficticios/dataframe.R", echo=F)
+source('https://raw.githubusercontent.com/GabrielGabes/Codigos-uteis/main/Gerador%20de%20Dados%20Ficticios/dataframe.R')
+
+######################################################################################
+
+#### Compilando arquivos .RMD ####
+pacman::p_load(knitr)
+knit("C:/Users/gabri/OneDrive/Documentos/Codigos úteis/R program/Funcoes.Rmd")
+#knit("~/Codigos úteis/R program/Graficos_GGplot.Rmd")
+source('https://raw.githubusercontent.com/GabrielGabes/Codigos-uteis/main/R%20program/Graficos_GGplot.R')
+
+######################################################################################
+
+#### BIBLIOTECAS SEMPRE USADAS ####
+pacman::p_load(
+  readxl, #Importar arquvios excel ==> read_excel(".xlsx)
+  skimr, #resumo dos dados do dataframe => skim(df)
+  
+  reshape2, #manipulação de formato de dataframe
+  stringr, #manipulação de strings
+  vegan, #transformação de variaveis
+  forcats, #manipulação de factor
+  
+  # Outros pacotes
+  naniar, #analise de dados ausentes
+  DescTools #canivete suiço
+)
+
+######################################################################################
+
+pacman::p_load(clipr) # captura dos dados => write_clip
+capture = function(tabela, col_names=TRUE, pontuacao=','){
+  tabela %>% print() %>% write_clip(dec = pontuacao, col.names = col_names)
+}
+
+'AMBIENTE PRONTO PARA TRABALHO'
