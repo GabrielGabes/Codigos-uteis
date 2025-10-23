4c501de32bc14a7853ea23ca2bdb82cd8514dce1
Gabriel Silva dos Anjos <Gabriel_s_anjos@yahoo.com>
2025-10-23 13:16:02 -0300

    alteracoes necessaria
    kQBJDNLKWRJA

---
+++ Convert Rmd to ipynb/Rmd_to_ipynb.R
@@ -0,0 +1,5 @@
+## install and load rmd2jupyter
+devtools::install_github("mkearney/rmd2jupyter")
+library(rmd2jupyter)
+
+rmd2jupyter('C:/Users/gabri/OneDrive/Documentos/Outros/Convert Rmd to ipynb/teste.Rmd')
